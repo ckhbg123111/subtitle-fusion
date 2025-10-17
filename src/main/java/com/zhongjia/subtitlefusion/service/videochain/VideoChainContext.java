84c63e9054a33611dacd23f6b34aa41a32f065ee
Japan Account <3170105539@zju.edu.cn>
2025-10-17 15:25:14 +0800

    注解

--- src/main/java/com/zhongjia/subtitlefusion/service/videochain/VideoChainContext.java
+++ src/main/java/com/zhongjia/subtitlefusion/service/videochain/VideoChainContext.java
@@ -1,11 +1,15 @@
 package com.zhongjia.subtitlefusion.service.videochain;
 
 import com.zhongjia.subtitlefusion.model.VideoChainRequest;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@RequiredArgsConstructor
 public class VideoChainContext {
 
     private final String taskId;
@@ -23,83 +27,6 @@ public class VideoChainContext {
     private Double bgmFadeOutSec;
 
     private Path finalOut;
-
-    public VideoChainContext(String taskId, VideoChainRequest request) {
-        this.taskId = taskId;
-        this.request = request;
-    }
-
-    public String getTaskId() {
-        return taskId;
-    }
-
-    public VideoChainRequest getRequest() {
-        return request;
-    }
-
-    public Path getWorkDir() {
-        return workDir;
-    }
-
-    public void setWorkDir(Path workDir) {
-        this.workDir = workDir;
-    }
-
-    public List<Path> getTempFiles() {
-        return tempFiles;
-    }
-
-    public List<Path> getSegmentOutputs() {
-        return segmentOutputs;
-    }
-
-    public Path getBgm() {
-        return bgm;
-    }
-
-    public void setBgm(Path bgm) {
-        this.bgm = bgm;
-    }
-
-    public boolean isAnySegHasAudio() {
-        return anySegHasAudio;
-    }
-
-    public void setAnySegHasAudio(boolean anySegHasAudio) {
-        this.anySegHasAudio = anySegHasAudio;
-    }
-
-    public Double getBgmVolume() {
-        return bgmVolume;
-    }
-
-    public void setBgmVolume(Double bgmVolume) {
-        this.bgmVolume = bgmVolume;
-    }
-
-    public Double getBgmFadeInSec() {
-        return bgmFadeInSec;
-    }
-
-    public void setBgmFadeInSec(Double bgmFadeInSec) {
-        this.bgmFadeInSec = bgmFadeInSec;
-    }
-
-    public Double getBgmFadeOutSec() {
-        return bgmFadeOutSec;
-    }
-
-    public void setBgmFadeOutSec(Double bgmFadeOutSec) {
-        this.bgmFadeOutSec = bgmFadeOutSec;
-    }
-
-    public Path getFinalOut() {
-        return finalOut;
-    }
-
-    public void setFinalOut(Path finalOut) {
-        this.finalOut = finalOut;
-    }
 }
 
 
