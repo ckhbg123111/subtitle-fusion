559bf35424ff02fbcbc06149145a672cbd36545c
Kehao Chen <3170105539@zju.edu.cn>
2025-11-07 15:47:47 +0800

    新增预置效果需求描述

--- src/main/java/com/zhongjia/subtitlefusion/controller/CapCutScriptDrivenController.java
+++ src/main/java/com/zhongjia/subtitlefusion/controller/CapCutScriptDrivenController.java
@@ -21,6 +21,10 @@ public class CapCutScriptDrivenController {
 
     @PostMapping(value = "/capcut-gen", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public CapCutGenResponse submit(@RequestBody SubtitleFusionV2Request request)  {
+        // 调用端暂时不传输花字效果和文字模板
+        // todo 当没有keywords时，随机选择一个花字或者选择一个文字模板，花字和文字模板见MCP接口文档
         return draftWorkflowService.generateDraft(request);
     }
+
+
 }
